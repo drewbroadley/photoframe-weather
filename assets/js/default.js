a90af2de1919e74e345f7252a16a847d5ab25811
Drew Broadley <drew@penny.co.nz>
2016-08-13 17:21:35 +1200

    Updates

--- assets/js/default.js
+++ assets/js/default.js
@@ -4,11 +4,13 @@ $(document).ready(function() {
   var hour = (min * 60);
   var day = (hour * 24);
 
+  var photo_url;
+
   getWeather(); //Get the initial weather.
 
   getPhoto(); //Get the initial photo.
 
-  setInterval(getWeather, min); //Update the weather every 10 minutes.
+  setInterval(getWeather, min * 15); //Update the weather every 10 minutes.
 
   setInterval(getPhoto, min/4);
 
@@ -23,13 +25,23 @@ function reloadPage()
 
 function getPhoto()
 {
+
   $.ajax({
     url: "/api/photo/",
     success: function(photo)
     {
       console.log(photo);
 
-      $("body").css('background-image', 'url(' + photo + ')')
+      photo_url = photo;
+
+      img = new Image();
+      img.onload = function () {
+        $("body").css('background-image', 'url(' + photo_url + ')')
+      }
+
+      img.src = photo_url;
+
+
     }
   });
 
@@ -40,10 +52,12 @@ function getWeather() {
     url: "/api/weather/",
     success: function(weather)
     {
-      $("div#weather .temp span").text(weather.info.temp);
+      $("div#weather .temp span.degrees").text(weather.info.temp);
 
-      $("div#weather .temp-high-low span.low").text(weather.forecast.today.min);
-      $("div#weather .temp-high-low span.high").text(weather.forecast.today.max);
+      $("div#weather .temp-feels-high-low span.feels").text(weather.info.windChill);
+
+      //$("div#weather .temp-feels-high-low span.low").text(weather.forecast.today.min);
+      $("div#weather .temp-feels-high-low span.high").text(weather.forecast.today.max);
 
       $("div#weather .desc span").text(weather.forecast.today.forecast);
 
